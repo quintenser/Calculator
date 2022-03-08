6c623d71eb4f8013db92c2fc49b77f6f51b6a112
Quinten <s1163486@student.windesheim.nl>
2022-03-08 16:33:48 +0100

    Proof of Concept

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -20,9 +20,100 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        public int X = 0;
+        public int Y = 0;
+        public int calculatedResult = 0;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void parseInput(object sender, RoutedEventArgs e)
+        {   
+            try { X = Int32.Parse(inputX.Text); }
+            catch
+            {
+                string msg = "X is geen getal.";
+                string title = "Verkeerde invoer";
+                MessageBox.Show(msg, title);
+            }
+
+            try { Y = Int32.Parse(inputY.Text); }
+            catch
+            {
+                string msg = "Y is geen getal.";
+                string title = "Verkeerde invoer";
+                MessageBox.Show(msg, title);
+            }
+        }
+        private void parseResult(object sender, RoutedEventArgs e)
+        {
+            result.Text = Convert.ToString(calculatedResult);
+        }
+        private void NumY_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void NumX_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void add(object sender, RoutedEventArgs e)
+        {
+            parseInput(sender, e);
+            calculatedResult = X + Y;
+            parseResult(sender, e);
+        }
+
+        private void subtract(object sender, RoutedEventArgs e)
+        {
+            parseInput(sender, e);
+            calculatedResult = X - Y;
+            parseResult(sender, e);
+        }
+        private void multiply(object sender, RoutedEventArgs e)
+        {
+            parseInput(sender, e);
+            calculatedResult = X * Y;
+            parseResult(sender, e);
+        }
+        private void divide(object sender, RoutedEventArgs e)
+        {
+            parseInput(sender, e);
+            calculatedResult = X / Y;
+            parseResult(sender, e);
+        }
+        private void root(object sender, RoutedEventArgs e)
+        {
+            parseInput(sender, e);
+            var doubleX = Convert.ToDouble(X);
+            calculatedResult = (int)Math.Sqrt(doubleX);
+            parseResult(sender, e);
+        }
+        private void expone(object sender, RoutedEventArgs e)
+        {
+            parseInput(sender, e);
+            var exponeX = Convert.ToDouble(X);
+            var exponeY = Convert.ToDouble(Y);
+            calculatedResult = (int)Math.Pow(exponeX, exponeY);
+            parseResult (sender, e);
         }
     }
 }
